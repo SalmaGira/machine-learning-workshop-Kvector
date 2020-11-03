95dab54646f6652feb6a49ae38acce458e41f432
meryacine <omar.groza@gmail.com>
2020-11-03 08:48:14 +0200

    multivar model

--- Sessions/Session 3/python/multivar.py
+++ Sessions/Session 3/python/multivar.py
@@ -25,11 +25,17 @@ def grad (theta, alpha, X, Y):
 
 if __name__ == "__main__":
     
-    kc = np.genfromtxt('./data/king_county.csv', delimiter = ',')
-    kc = np.delete(kc, 0, axis=0)
+    file_name = input('file_name = ')
+    kc = np.genfromtxt('./data/'+file_name+'.csv', delimiter = ',')
+    kc = np.delete(kc, 0, axis = 0)
     
-    features = kc[:,0:-1]
+    #features = kc[:,0:-1]  #unscaled features
+    features = (kc[:,0:-1]-kc[:,0:-1].mean(axis=0))/(kc[:,0:-1].max(axis=0)-kc[:,0:-1].min(axis=0))  #features scaled
     values = kc[:,-1]
+    
+    ##mods
+    plt.scatter(features,values)
+    ##end
 
     theta = np.zeros(features[0].size + 1)
 
@@ -38,4 +44,9 @@ if __name__ == "__main__":
 
     for i in range(iterations):
         theta = grad(theta, alpha, features, values)
-        print('iteration #{}: J = {}'.format(i+1, J(theta, features, values)))
+        print('iteration #{}: J = {} , theta = {}'.format(i+1, J(theta, features, values), theta))
+        ##mods
+        plt.plot(features,features[:,0]*theta[1]+theta[0])
+        plt.pause(0.1)
+        ##end
+    plt.show()
