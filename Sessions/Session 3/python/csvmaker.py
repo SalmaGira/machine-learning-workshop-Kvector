3bcd4c691c9c6ef104119c6565d3f8e614266478
meryacine <omar.groza@gmail.com>
2020-11-09 21:32:40 +0200

    mostly

--- Sessions/Session 3/python/csvmaker.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-arr1 = np.arange(30,dtype=int)
-arr2 = arr1*5 +7
-
-np.savetxt('./data/test.csv',np.array([arr1,arr2]).transpose(),delimiter=',')
---
+++ Sessions/Session 3/python/data/csvmaker.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+arr1 = np.arange(30,dtype=int)
+arr2 = arr1*5+7
+
+np.savetxt('./test.csv',np.array([arr1,arr2]).transpose(),delimiter=',')
