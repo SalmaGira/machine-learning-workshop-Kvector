d4c6fd72382b8503d53bb8e578a8ab2c689641e9
meryacine <omar.groza@gmail.com>
2020-12-23 15:58:30 +0200

    all

--- Sessions/Session 3/python/multivar_loops.py
+++ Sessions/Session 3/python/multivar_loops.py
@@ -1,26 +1,28 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
-
-def get_X(X, i, j):
-    return 1 if j == 0 else X[i][j-1]
-
-def h(theta, X_i):
-    return np.sum(theta[0] + theta[1:X_i.size+1] * X_i)
+def h(theta, X):
+    n = X.size + 1
+    hy = 0
+    for i in range(n):
+        hy += theta[i]*X[i]
+    return hy
 
 def J(theta, X, Y):
+    m = Y.size
     sigma = 0
-    for i in range(Y.size):
+    for i in range(m):
         sigma += (h(theta, X[i]) - Y[i])**2
-    return sigma/(2*Y.size)
+    return sigma/(2*m)
 
 def grad (theta, alpha, X, Y):
-    new_theta = np.empty(X[0].size+1)
-    for j in range(X[0].size+1):
+    m = Y.size # number of rows = number of data samples
+    n = X[0].size + 1 # number of feautres + 1 // 1 => theta(zero)
+    new_theta = np.empty(n)
+    for j in range(n):
         sigma = 0
-        for i in range(Y.size):
-            sigma += (h(theta, X[i]) - Y[i]) * get_X(X ,i, j)
-        new_theta[j] = theta[j] - (alpha/Y.size) * sigma
+        for i in range(m):
+            sigma += (h(theta, X[i]) - Y[i]) * X[i,j]
+        new_theta[j] = theta[j] - (alpha/m) * sigma
     return new_theta
 
 if __name__ == "__main__":
@@ -29,11 +31,12 @@ if __name__ == "__main__":
     kc = np.genfromtxt('./data/'+file_name+'.csv', delimiter = ',')
     kc = np.delete(kc, 0, axis = 0)
     
-    #features = kc[:,0:-1]  #unscaled features
-    features = (kc[:,0:-1]-kc[:,0:-1].mean(axis=0))/(kc[:,0:-1].max(axis=0)-kc[:,0:-1].min(axis=0))  #features scaled
+    features = kc[:,0:-1]  #unscaled features
     values = kc[:,-1]
-
-    theta = np.zeros(features[0].size + 1)
+    
+    features = np.insert(features, 0, 1, axis=1)
+    
+    theta = np.ones(features[0].size + 1)
 
     iterations = int(input("num_of_iterations = "))
     alpha = float(input("alpha = "))
@@ -41,3 +44,11 @@ if __name__ == "__main__":
     for i in range(iterations):
         theta = grad(theta, alpha, features, values)
         print('iteration #{}: J = {} , theta = {}'.format(i+1, J(theta, features, values), theta))
+    
+    
+    
+    
+    
+    
+    
+    #features = (kc[:,0:-1]-kc[:,0:-1].mean(axis=0))/(kc[:,0:-1].max(axis=0)-kc[:,0:-1].min(axis=0))  #features scaled
